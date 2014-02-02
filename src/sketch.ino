deb7261d8e358f8810729dfb05be0950ac2de7a0
Alex Beard <bearda@purdue.edu>
2014-02-02 15:03:04 -0500

    Now measuring relative to ground

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,31 +1,29 @@
 # define ANALOG_INPUT_0 5 
-# define ANALOG_INPUT_1 4 
 # define RATIO 0.00489
+int i;
 int pin_reading_0;
-int pin_reading_1;
+int pin_reading_0_sum_of_readings;
 int diff;
 float output;
+int temp;
 
 void setup()
 {
     pinMode(ANALOG_INPUT_0,INPUT);
-    pinMode(ANALOG_INPUT_1,INPUT);
     Serial.begin(9600);
 }
 
 void loop()
 {
-    delay(2000);
-    Serial.println("Reading voltage...");
-    pin_reading_0 = analogRead(ANALOG_INPUT_0);
-    pin_reading_1 = analogRead(ANALOG_INPUT_1);
-    Serial.println(pin_reading_0);
-    Serial.println(pin_reading_1);
-    Serial.println("Comparing values...");
-    diff = pin_reading_0 - pin_reading_1;
-    Serial.println("Done!");
-    Serial.println("Preparing output...");
-    output = diff * RATIO;
+    delay(200);
+    pin_reading_0_sum_of_readings = 0;
+    for (i=0;i<=10;i++) {
+        delay(100);
+        temp = analogRead(ANALOG_INPUT_0);
+        pin_reading_0_sum_of_readings += analogRead(ANALOG_INPUT_0);
+    }
+    pin_reading_0 = pin_reading_0_sum_of_readings / i;
+    output = pin_reading_0 * RATIO;
     Serial.print(output);
     Serial.println(" Volts");
 }
